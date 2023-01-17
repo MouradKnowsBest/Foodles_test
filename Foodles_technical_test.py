1aec693524a7e24aee1658c6c14bff1b6eb1ea1b
Mourad.KnowsBest <mourad.ab01@gmail.com>
2023-01-17 23:06:46 +0100

    added two simple tests using pytest

--- Foodles_technical_test.py
+++ Foodles_technical_test.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from itertools import islice
-import unittest
+import pytest
 
 def take_n_elements_from_dict(n, dict):
     return list(islice(dict, n))
@@ -9,6 +9,7 @@ def frequency_of_word_in_sentence(sentence, n):
     
     if (not sentence) or (sentence == ""):
         print("sorry there is no sentence entered !  Please enter a sentence")
+        return "sorry there is no sentence entered !  Please enter a sentence"
     else:
         sorted_sentence_list = sorted([word for word in sentence.split()])
         
@@ -19,7 +20,20 @@ def frequency_of_word_in_sentence(sentence, n):
         return take_n_elements_from_dict(n, sorted_and_ordered_sentence_list.items())
         
 
-    
+def returns_the_exact_frequency_of_words():
+    assert frequency_of_word_in_sentence("baz bar foo foo zblah zblah zblah baz toto bar", 3) == [
+   ("zblah", 3),
+   ("bar", 2),
+   ("baz", 2),
+]
+
+def returns_the_error_message_if_no_text():
+    assert frequency_of_word_in_sentence("", 3) == "sorry there is no sentence entered !  Please enter a sentence"
+
+returns_the_exact_frequency_of_words()
+
+returns_the_error_message_if_no_text()
+
 if __name__ == "__main__":
     frequency_of_word_in_sentence("", 3)
     frequency_of_word_in_sentence("baz bar foo foo zblah zblah zblah baz toto bar", 3)
